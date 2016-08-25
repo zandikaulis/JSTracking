1365bd7fc191e692bdde6ad22b19fc46e27d7b9f
derrod <hitreg@really.dice.wtf>
2016-08-25 22:31:06 +0200

    Updated script Now it's even more flexible. Simlpe regex function to find things that change the url fairly often

--- jstrackr.py
+++ jstrackr.py
@@ -2,14 +2,28 @@
 
 import requests
 import jsbeautifier
+import re
 
-files = [
-    {'url': 'https://player.twitch.tv/js/player.js',
-     'filename': 'player.js'}
-]
 
-for jsfile in files:
-    print "Downloading", jsfile['url'], "to file", jsfile['filename']
-    src = requests.get(jsfile['url']).content
-    beatuified = jsbeautifier.beautify(src)
-    open('js/%s' % jsfile['filename'], 'wb').write(beatuified)
+def get_url_from_page(url, pat):
+    # Why not, easy as fuck
+    name = re.search(pat, requests.get(url).content).group(1)
+    return url + name
+
+if __name__ == '__main__':
+    files = [
+        {'url': 'https://player.twitch.tv/js/player.js',
+         'filename': 'player.js'},
+        {'url': 'https://www.twitch.tv/site_options.js',
+         'filename': 'site_options.js'},
+        {'url': get_url_from_page("https://www.twitch.tv", r'"(.*/global-[a-zA-Z0-9]+.js)'),
+         'filename': 'global.js'},
+        {'url': get_url_from_page("https://www.twitch.tv", r'"(.*/application-[a-zA-Z0-9]+.js)'),
+         'filename': 'global.js'}
+    ]
+
+    for jsfile in files:
+        print "Downloading", jsfile['url'], "to file", jsfile['filename']
+        src = requests.get(jsfile['url']).content
+        beatuified = jsbeautifier.beautify(src)
+        open('js/%s' % jsfile['filename'], 'wb').write(beatuified)
