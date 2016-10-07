ee1ba8e3f9f42cef3b245ae7a3cdd4dc5d0c21b2
derrod <hitreg@really.dice.wtf>
2016-10-07 20:15:59 +0200

    Update 07.10.2016 20:15

--- js/site_options.js
+++ js/site_options.js
@@ -821,10 +821,10 @@ window.SiteOptions = {
         "e7516730-b94f-4d46-894e-c18612aab270": {
             "name": "coview recommendations a/b testing",
             "groups": [{
-                "weight": 0,
+                "weight": 100,
                 "value": "control"
             }, {
-                "weight": 100,
+                "weight": 0,
                 "value": "experiment"
             }]
         },
