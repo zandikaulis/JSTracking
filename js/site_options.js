dfe10e8f864697408d5a2974d7270f7cf95d874a
derrod <hitreg@really.dice.wtf>
2016-10-08 02:00:58 +0200

    Update 08.10.2016 02:00

--- js/site_options.js
+++ js/site_options.js
@@ -153,13 +153,13 @@ window.SiteOptions = {
         "15dde59b-96ad-4040-beba-4c73039d16ff": {
             "name": "Transcode_Quality_Experiment_2",
             "groups": [{
-                "weight": 90,
+                "weight": 70,
                 "value": "regular"
             }, {
-                "weight": 5,
+                "weight": 15,
                 "value": "plus"
             }, {
-                "weight": 5,
+                "weight": 15,
                 "value": "nonplus"
             }]
         },
