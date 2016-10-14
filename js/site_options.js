3e0a83dc0a7c1f3c5e2cc5fa7a5161c6e84ea1ac
derrod <hitreg@really.dice.wtf>
2016-10-15 00:31:01 +0200

    Update 15.10.2016 00:31

--- js/site_options.js
+++ js/site_options.js
@@ -762,10 +762,10 @@ window.SiteOptions = {
         "c926728f-d1d3-4b09-b868-db3e2aa000cc": {
             "name": "In-Player Recommendations",
             "groups": [{
-                "weight": 0,
+                "weight": 100,
                 "value": "yes"
             }, {
-                "weight": 100,
+                "weight": 0,
                 "value": "no"
             }]
         },
