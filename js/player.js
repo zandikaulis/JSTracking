da2ebcf1235fad5e9b74aacb1e729004a594630c
derrod <hitreg@really.dice.wtf>
2016-10-18 20:45:56 +0200

    Update 18.10.2016 20:45

--- js/player.js
+++ js/player.js
@@ -7838,7 +7838,7 @@
                     S = _.get(!1),
                     w = _.get(!0);
                 h = {
-                    app_version: "2016.10.18-175611+5c57b2d6ce00fa487d5f7b90b7610b624a691db6",
+                    app_version: "2016.10.18-180318+cb783af8517cd3760240ee9fa0ba7c4c9609ed25",
                     flash_version: g,
                     url: v.href,
                     host: v.host,
