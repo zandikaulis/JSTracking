4bf75809a76c09e742f0e277f686ddc2c3b5df04
derrod <hitreg@really.dice.wtf>
2016-12-13 00:46:03 +0100

    Update 13.12.2016 00:46

--- js/site_options.js
+++ js/site_options.js
@@ -144,10 +144,10 @@ window.SiteOptions = {
         "0670f0ea-3b5c-41a8-946d-114ef4a42540": {
             "name": "INTL_I18N",
             "groups": [{
-                "weight": 100,
+                "weight": 99,
                 "value": "control"
             }, {
-                "weight": 0,
+                "weight": 1,
                 "value": "enabled"
             }]
         },
