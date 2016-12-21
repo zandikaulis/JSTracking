83969e5057782d568367067ec034d5ba541b8f62
derrod <hitreg@really.dice.wtf>
2016-12-21 23:16:09 +0100

    Update 21.12.2016 23:16

--- js/site_options.js
+++ js/site_options.js
@@ -502,7 +502,7 @@ window.SiteOptions = {
         "5380a8cf-e5cf-497e-9c6c-16cb1c881e0c": {
             "name": "WATCH_NOW_RIGHTBAR",
             "groups": [{
-                "weight": 80,
+                "weight": 95,
                 "value": "control"
             }, {
                 "weight": 5,
