73774be8037926f25ee760b104001aa6e4229927
derrod <hitreg@really.dice.wtf>
2017-01-09 22:31:07 +0100

    Update 09.01.2017 22:31

--- js/site_options.js
+++ js/site_options.js
@@ -166,10 +166,10 @@ window.SiteOptions = {
         "0670f0ea-3b5c-41a8-946d-114ef4a42540": {
             "name": "INTL_I18N",
             "groups": [{
-                "weight": 100,
+                "weight": 90,
                 "value": "control"
             }, {
-                "weight": 0,
+                "weight": 10,
                 "value": "enabled"
             }]
         },
