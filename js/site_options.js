42caeee71d47ccda78a3f7810c126697cbb17f3e
derrod <hitreg@really.dice.wtf>
2017-01-19 04:31:11 +0100

    Update 19.01.2017 04:31

--- js/site_options.js
+++ js/site_options.js
@@ -339,10 +339,7 @@ window.SiteOptions = {
         "33ad61d9-3433-4073-b091-83251b29ea46": {
             "name": "CVP-50",
             "groups": [{
-                "weight": 5,
-                "value": "1.3.0"
-            }, {
-                "weight": 95,
+                "weight": 100,
                 "value": "1.1.117"
             }]
         },
