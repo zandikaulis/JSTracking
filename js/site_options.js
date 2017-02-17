875f5e4d2bb429fb02cedf62a469c2522861ec7e
derrod <hitreg@really.dice.wtf>
2017-02-17 22:15:39 +0100

    Update 17.02.2017 22:15

--- js/site_options.js
+++ js/site_options.js
@@ -1036,10 +1036,10 @@ window.SiteOptions = {
         "b2925d54-cebb-4e1d-a50d-9dd2b1ea850e": {
             "name": "onsite_notifications",
             "groups": [{
-                "weight": 90,
+                "weight": 100,
                 "value": "control"
             }, {
-                "weight": 10,
+                "weight": 0,
                 "value": "variant1"
             }]
         },
