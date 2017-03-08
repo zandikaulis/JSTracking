2f46fbb83cabf9b92a8cf49e1721960a53f84685
derrod <hitreg@really.dice.wtf>
2017-03-08 21:15:39 +0100

    Update 08.03.2017 21:15

--- js/site_options.js
+++ js/site_options.js
@@ -1066,13 +1066,13 @@ window.SiteOptions = {
         "af8f152a-f618-416e-8b79-d6ac30479018": {
             "name": "PERPETUA",
             "groups": [{
-                "weight": 0,
+                "weight": 10,
                 "value": "early"
             }, {
                 "weight": 0,
                 "value": "mid"
             }, {
-                "weight": 100,
+                "weight": 90,
                 "value": "control"
             }]
         },
