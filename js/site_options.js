a760bbd9d0eb385f1ec1e0187cd739ae9d6ef6e2
derrod <hitreg@really.dice.wtf>
2017-03-13 21:30:43 +0100

    Update 13.03.2017 21:30

--- js/site_options.js
+++ js/site_options.js
@@ -206,10 +206,7 @@ window.SiteOptions = {
         "0c22fa7f-9b7d-4c6a-9b63-1f70c0af3df2": {
             "name": "CVP-Mar-2017",
             "groups": [{
-                "weight": 48,
-                "value": "1.3.5"
-            }, {
-                "weight": 40,
+                "weight": 88,
                 "value": "1.5.1"
             }, {
                 "weight": 2,
