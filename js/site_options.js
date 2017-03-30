0245c7bdf4e97b6ac35fe1e688e567d20024794c
derrod <hitreg@really.dice.wtf>
2017-03-30 23:45:46 +0200

    Update 30.03.2017 23:45

--- js/site_options.js
+++ js/site_options.js
@@ -1342,14 +1342,14 @@ window.SiteOptions = {
         "d48fe249-fd73-4c8f-bd17-415b70dc3708": {
             "name": "chomperz",
             "groups": [{
+                "weight": 0,
+                "value": "five"
+            }, {
                 "weight": 100,
-                "value": "plain"
+                "value": "control"
             }, {
                 "weight": 0,
-                "value": "jalapeno"
-            }, {
-                "weight": 0,
-                "value": "cheese"
+                "value": "fifteen"
             }]
         },
         "d4d938f7-bdc2-42cd-afd0-f434e4192c29": {
