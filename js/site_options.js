fcd7697fa6f93c016fa0985740627f2ccef1f7c4
derrod <hitreg@really.dice.wtf>
2017-04-18 23:45:45 +0200

    Update 18.04.2017 23:45

--- js/site_options.js
+++ js/site_options.js
@@ -1300,7 +1300,7 @@ window.SiteOptions = {
                 "weight": 1,
                 "value": "yes"
             }, {
-                "weight": 4,
+                "weight": 1,
                 "value": "no"
             }]
         },
