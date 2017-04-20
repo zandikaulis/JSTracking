49fd8b0d8df9d6aa4cae8306aa9c9a5ce660a69b
derrod <hitreg@really.dice.wtf>
2017-04-20 20:30:45 +0200

    Update 20.04.2017 20:30

--- js/site_options.js
+++ js/site_options.js
@@ -890,10 +890,10 @@ window.SiteOptions = {
         "88ad36bd-f5ba-4c8c-8d06-30e486cd5d07": {
             "name": "clips_scared_to_be_lonely",
             "groups": [{
-                "weight": 50,
+                "weight": 0,
                 "value": "control"
             }, {
-                "weight": 50,
+                "weight": 1,
                 "value": "variant1"
             }]
         },
