2441c361a03adc122719c3c5a7993a5a9fdb63d8
derrod <hitreg@really.dice.wtf>
2017-05-16 03:00:50 +0200

    Update 16.05.2017 03:00

--- js/site_options.js
+++ js/site_options.js
@@ -770,7 +770,7 @@ window.SiteOptions = {
             "name": "spade_url",
             "groups": [{
                 "weight": 1,
-                "value": "//video-edge-8a9219.sjc01.hls.ttvnw.net"
+                "value": "//video-edge-a1d22e.sjc01.hls.ttvnw.net"
             }]
         },
         "4d081a33-0d0f-4b44-bfde-cf07c358915f": {
