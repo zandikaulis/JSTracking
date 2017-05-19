3594f12306d5a064af3f6c748c3fadd503939ed9
derrod <hitreg@really.dice.wtf>
2017-05-19 09:15:43 +0200

    Update 19.05.2017 09:15

--- js/extensions.js
+++ js/extensions.js
@@ -141,7 +141,7 @@ body >
 
     <
     script src = "//www-cdn.jtvnw.net/assets/emberhelper.js" > < /script> <
-    script src = "//web-cdn.ttvnw.net/emberapp-f50def6cc69d30ed264167125700b13d.js" > < /script> <
+    script src = "//web-cdn.ttvnw.net/emberapp-abb22081818f3208a5d288c37641464e.js" > < /script> <
     script src = "//www-cdn.jtvnw.net/tmilibs/tmi-v3.js" > < /script>
 
 
