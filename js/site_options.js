0bcb817de2ab574248706d76f66f604cc026c3aa
derrod <hitreg@really.dice.wtf>
2017-06-12 18:00:49 +0200

    Update 12.06.2017 18:00

--- js/site_options.js
+++ js/site_options.js
@@ -1028,10 +1028,10 @@ window.SiteOptions = {
         "78360f5d-4ace-420d-8bca-df0d146ffdee": {
             "name": "CHANNEL_SIGNUP_COLUMN",
             "groups": [{
-                "weight": 10,
+                "weight": 1,
                 "value": "treatment"
             }, {
-                "weight": 90,
+                "weight": 99,
                 "value": "control"
             }]
         },
