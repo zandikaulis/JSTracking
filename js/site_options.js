4349867d41217a92a7233939d2bdde0f136cac4e
derrod <hitreg@really.dice.wtf>
2017-06-29 19:30:43 +0200

    Update 29.06.2017 19:30

--- js/site_options.js
+++ js/site_options.js
@@ -545,10 +545,10 @@ window.SiteOptions = {
         "30a7dfaa-e3d2-477e-a4f1-28e2ffb80e72": {
             "name": "NEWSFEED_RECOMMENDATIONS",
             "groups": [{
-                "weight": 50,
+                "weight": 40,
                 "value": "control"
             }, {
-                "weight": 50,
+                "weight": 60,
                 "value": "variant"
             }]
         },
