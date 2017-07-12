ccba1d3a6b11a7082ef85b2c78b886c02f6ec4b8
derrod <hitreg@really.dice.wtf>
2017-07-12 20:45:43 +0200

    Update 12.07.2017 20:45

--- js/site_options.js
+++ js/site_options.js
@@ -1171,10 +1171,10 @@ window.SiteOptions = {
                 "weight": 50,
                 "value": "variant1"
             }, {
-                "weight": 49,
+                "weight": 50,
                 "value": "control"
             }, {
-                "weight": 1,
+                "weight": 0,
                 "value": "variant2"
             }]
         },
