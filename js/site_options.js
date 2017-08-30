31e5f89e8cf939dbfa4b3a11cc7a1d1310961cea
derrod <hitreg@really.dice.wtf>
2017-08-30 22:00:48 +0200

    Update 30.08.2017 22:00

--- js/site_options.js
+++ js/site_options.js
@@ -1,3 +1,32 @@
-< html > < body > < h1 > 504 Gateway Time - out < /h1>
-The server didn 't respond in time. <
-    /body></html >
+< !DOCTYPE html >
+    <
+    html >
+    <
+    head >
+    <
+    title > Error < /title> <
+    style >
+    body {
+        width: 35e m;
+        margin: 0 auto;
+        font - family: Tahoma,
+        Verdana,
+        Arial,
+        sans - serif;
+    } <
+    /style> <
+    /head> <
+    body >
+    <
+    h1 > An error occurred. < /h1> <
+    p > Sorry, the page you are looking
+for is currently unavailable. < br / >
+    Please
+try again later. < /p> <
+    p > If you are the system administrator of this resource then you should check
+the < a href = "http://nginx.org/r/error_log" > error log < /a> for details.</p >
+    <
+    p > < em > Faithfully yours, nginx. < /em></p >
+    <
+    /body> <
+    /html>
