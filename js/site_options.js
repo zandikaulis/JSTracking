3b762fc7bae2ff5939455b5d6185772103825bed
derrod <hitreg@really.dice.wtf>
2017-09-04 03:00:47 +0200

    Update 04.09.2017 03:00

--- js/site_options.js
+++ js/site_options.js
@@ -1016,11 +1016,11 @@ window.SiteOptions = {
         },
         "4badc757-13a7-468c-99b6-e42aef7fc286": {
             "name": "spade_url",
-            "v": 1947,
+            "v": 1948,
             "t": 1,
             "groups": [{
                 "weight": 1,
-                "value": "////video-edge-acd23d.sjc01.hls.ttvnw.net/v1/playlist/Cgnl4keesM8fO2RY7fpOum9t-iqJIsh0YrliZc16fWjWsuZ6gbF1wGYKiGAho3xhRq3hvh_-Vi_N0OQ1p0wX9SaEe_bCku4pYiX1vYus0ruTJs_C-YxE3N-Lr40FiLYfaKA9-OOfgWdDgy-GJRiFxrw3oLNMIxCUjNpQzksuarMTCL-nJf-QfphaF42qBwo5FZYAOxCsPzMCKCeCHhzBhMwWJYystRDftmliPHvdeaWhlCGsvFeXQPANUq8E8dbmOgtplkzT43DJ-e348PLJdfUhuTZ2x6UGLCWO11bD1Z-enNAIyaX1XBcF-As8HOlXa5Zo-_bQ3kJuOufaVN1Us-DSA7IP7D93UwLvG11Pjf6Ah-ZDSZVAh6pcFFPotrKohetXS6Je4Jxa_NRBPRTFtx2qektO4KwERNhTPnZ0rWIAkik1T-miaR-D6hYJpUlwdInebmW-.m3u8"
+                "value": "////video-edge-b33e8d.sjc01.hls.ttvnw.net/v1/playlist/Cglj1_Kj0TiR-LnPxt4ExbLH6mnoIP9nKdEuCv8H_qUbG9V4uWSOc9i-PFlvZ_6wl885EJroG53vTmCF3EOAJ7fwt_qORrCwzzwKDoplr4lM5yLHHzfDavk_DBQ1xRy9R9omrDsJLJrtKwYtlQp-lpTPxVKQRgERdY196_2Kl_RBfl9mfOGAz5a7FoN2MK0HALkxILORqvt2lyDjZvv4YEpgpHZgSTHH-Kj0EXBFbKgFnNgTlTZCS_ZdgRwAE-j1EYwgDXUH3fZiO-6uS2IIXMBOO57bS0gTsxJWIX-cD6I-NeJZmKkx1Y0AydBZnlrqqQm_wwaZV8xNWahY5M_13juf2rq6otN-XjCShUl3p8vw1K8wr-eGvojJxlUcwB7Wyp4FN_o2UxAWzSQaKrIOtzgjUujtmw2wZW8O8fRcSv8DwGShB.m3u8"
             }]
         },
         "4bdb6678-2626-466f-bb25-e566dec11354": {
