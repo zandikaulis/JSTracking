9a747e09f4bac9ab8797a3e5c5d21cd38781fff7
derrod <hitreg@really.dice.wtf>
2017-09-27 02:15:42 +0200

    Update 27.09.2017 02:15

--- js/player.js
+++ js/player.js
@@ -38,7 +38,7 @@
             r[2] = o;
             var a = document.getElementsByTagName("head")[0],
                 s = document.createElement("script");
-            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".206373006bcbbb0d321b.js";
+            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".0f4173f6c4ee7450f63a.js";
             var u = setTimeout(n, 12e4);
             return s.onerror = s.onload = n, a.appendChild(s), o
         }, t.m = e, t.c = r, t.i = function(e) {
@@ -22303,7 +22303,7 @@
                     l = h.a(!0),
                     d = g.g() === g.n ? null : _;
                 return d = i.playerType === g.c ? y : d, {
-                    app_version: "2017.09.26-205922+ace9a70061486e3d3e19259d69598826ef4d55f2",
+                    app_version: "2017.09.26-234254+2b0470fde61f5c8c5935e674d95f476f9b1733b1",
                     flash_version: a,
                     referrer_url: s,
                     referrer_host: u.host,
