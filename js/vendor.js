7b914cdd3b62782ec73663712ea258a43384acd6
derrod <hitreg@really.dice.wtf>
2017-09-27 23:15:44 +0200

    Update 27.09.2017 23:15

--- js/vendor.js
+++ js/vendor.js
@@ -29,9 +29,9 @@
             return t.d(n, "a", n), n
         }, t.o = function(e, t) {
             return Object.prototype.hasOwnProperty.call(e, t)
-        }, t.p = "", t(t.s = 890)
+        }, t.p = "", t(t.s = 950)
     }({
-        387: function(e, t) {
+        405: function(e, t) {
             var e = void 0; /*! jQuery v1.12.4 | (c) jQuery Foundation | jquery.org/license */
             ! function(t, n) {
                 "object" == typeof e && "object" == typeof e.exports ? e.exports = t.document ? n(t, !0) : function(e) {
@@ -3343,8 +3343,8 @@
                 }, t || (e.jQuery = e.$ = pe), pe
             })
         },
-        890: function(e, t, n) {
-            e.exports = n(387)
+        950: function(e, t, n) {
+            e.exports = n(405)
         }
     })
 });
