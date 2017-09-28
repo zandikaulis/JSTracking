ae24fb5e4575d61c25454a985ef45d17fc9f1921
derrod <hitreg@really.dice.wtf>
2017-09-29 00:30:44 +0200

    Update 29.09.2017 00:30

--- js/player.js
+++ js/player.js
@@ -38,7 +38,7 @@
             r[2] = o;
             var a = document.getElementsByTagName("head")[0],
                 s = document.createElement("script");
-            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".86e182c23d8a575e4c27.js";
+            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".4cbbdca9c4088f36fc50.js";
             var u = setTimeout(n, 12e4);
             return s.onerror = s.onload = n, a.appendChild(s), o
         }, t.m = e, t.c = r, t.i = function(e) {
@@ -1930,6 +1930,12 @@
                         break;
                     case P.n:
                         y(_(e, r), r);
+                        break;
+                    case P.f:
+                        t(n.i(R.c)(e)), t(n.i(S.a)()), t(n.i(x.b)({
+                            contentType: x.f,
+                            contentId: e
+                        }));
                         break;
                     default:
                         t(n.i(S.a)()), t(n.i(x.b)({
@@ -22406,7 +22412,7 @@
                     l = h.a(!0),
                     d = g.h() === g.o ? null : _;
                 return d = i.playerType === g.c ? y : d, {
-                    app_version: "2017.09.28-195117+ef370f22fa9c5ccbed687ec396eba8b034b6c4a7",
+                    app_version: "2017.09.28-215737+69f72f1894f2b512f653f984c4dc55c405139224",
                     flash_version: a,
                     referrer_url: s,
                     referrer_host: u.host,
