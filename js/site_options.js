7a570f4a692340cb0f4f2656d244de47037dff0d
derrod <hitreg@really.dice.wtf>
2017-10-25 22:30:46 +0200

    Update 25.10.2017 22:30

--- js/site_options.js
+++ js/site_options.js
@@ -2503,13 +2503,13 @@ window.SiteOptions = {
         },
         "a88d97b7-ec93-4724-935e-e93f166df82f": {
             "name": "clips_feel_good",
-            "v": 2291,
+            "v": 2292,
             "t": 1,
             "groups": [{
-                "weight": 90,
+                "weight": 75,
                 "value": "control"
             }, {
-                "weight": 10,
+                "weight": 25,
                 "value": "variant1"
             }]
         },
