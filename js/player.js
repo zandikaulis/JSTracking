708d6f6c7e75433ca6e969a66b027fa1725c213f
derrod <hitreg@really.dice.wtf>
2017-10-25 21:30:39 +0200

    Update 25.10.2017 21:30

--- js/player.js
+++ js/player.js
@@ -38,7 +38,7 @@
             r[2] = a;
             var o = document.getElementsByTagName("head")[0],
                 s = document.createElement("script");
-            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".1584d7c5c57f988f0ff8.js";
+            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".6faa32f7d5841ff925d7.js";
             var u = setTimeout(n, 12e4);
             return s.onerror = s.onload = n, o.appendChild(s), a
         }, t.m = e, t.c = r, t.i = function(e) {
@@ -20612,7 +20612,7 @@
                     u = v.a(!0),
                     c = b.j() === b.o ? null : _;
                 return {
-                    app_version: "2017.10.25-162022+8b77262182e25da38b52100e3644339f185db8aa",
+                    app_version: "2017.10.25-185329+48494ff073b24377d22a9781b82567bf89cc1ba9",
                     flash_version: t,
                     referrer_url: i,
                     referrer_host: a.host,
