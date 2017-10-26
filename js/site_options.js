a5d3ad6fb4484d4a3d6f0d0a11c67d37461cc33f
derrod <hitreg@really.dice.wtf>
2017-10-26 19:30:47 +0200

    Update 26.10.2017 19:30

--- js/site_options.js
+++ js/site_options.js
@@ -1486,13 +1486,13 @@ window.SiteOptions = {
         },
         "5f94f02e-56fd-4681-bcba-95f37a7d83fa": {
             "name": "ZUORA_PHASE_1",
-            "v": 2299,
+            "v": 2300,
             "t": 1,
             "groups": [{
-                "weight": 100,
+                "weight": 0,
                 "value": "yes"
             }, {
-                "weight": 0,
+                "weight": 100,
                 "value": "no"
             }]
         },
