53a8f26d461832d6a684c7f23cf61caa5f5fa77f
derrod <hitreg@really.dice.wtf>
2017-11-01 22:30:48 +0100

    Update 01.11.2017 22:30

--- js/site_options.js
+++ js/site_options.js
@@ -3685,13 +3685,10 @@ window.SiteOptions = {
         },
         "ec70bd08-dbc8-4f48-9f60-d035fec3eb20": {
             "name": "CVP Player Core 2.0 Mediaplayer Experiment Aug 2017",
-            "v": 2330,
+            "v": 2338,
             "t": 1,
             "groups": [{
-                "weight": 50,
-                "value": "2.0.13"
-            }, {
-                "weight": 50,
+                "weight": 100,
                 "value": "2.0.16"
             }]
         },
