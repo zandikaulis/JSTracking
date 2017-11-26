7989ad300711349163979a9ee3d0952d8221c4d8
derrod <hitreg@really.dice.wtf>
2017-11-26 14:03:05 +0100

    Archive legacy JS (but continue tracking site_options for now)

--- jstrackr.py
+++ jstrackr.py
@@ -7,32 +7,32 @@ import random
 
 
 def get_url_from_page(url, pat):
-    content = requests.get(url, cookies={'twilight_beta_opt_out': '1'}).content
+    content = requests.get(url).content
     name = re.search(pat, content).group(1)
     return url + name
 
 if __name__ == '__main__':
     files = [
-        {'url': 'https://player.twitch.tv/js/player.js',
-         'filename': 'player.js'},
+        # {'url': 'https://player.twitch.tv/js/player.js',
+        #  'filename': 'player.js'},
         {'url': 'https://www.twitch.tv/site_options.js',
          'filename': 'site_options.js'},
-        {'url': 'https://web-cdn.ttvnw.net/global.js',
-         'filename': 'global.js'},
-        {'url': get_url_from_page("https://www.twitch.tv", r'"(.*/emberhelper-[a-zA-Z0-9]+.js)'),
-         'filename': 'emberhelper.js'},
-        {'url': get_url_from_page("https://player.twitch.tv/", r'"([a-z0-9]+/vendor.[a-zA-Z0-9]+.js)'),
-         'filename': 'vendor.js'},
-        {'url': 'https://web-cdn.ttvnw.net/emberapp.js',
-         'filename': 'emberapp.js'},
+        # {'url': 'https://web-cdn.ttvnw.net/global.js',
+        #  'filename': 'global.js'},
+        # {'url': get_url_from_page("https://www.twitch.tv", r'"(.*/emberhelper-[a-zA-Z0-9]+.js)'),
+        #  'filename': 'emberhelper.js'},
+        # {'url': get_url_from_page("https://player.twitch.tv/", r'"([a-z0-9]+/vendor.[a-zA-Z0-9]+.js)'),
+        #  'filename': 'vendor.js'},
+        # {'url': 'https://web-cdn.ttvnw.net/emberapp.js',
+        #  'filename': 'emberapp.js'},
         # {'url': 'https://web-cdn.ttvnw.net/extensions.js',
         #  'filename': 'extensions.js'},
         # {'url': 'https://web-cdn.ttvnw.net/game-details.js',
         #  'filename': 'game-details.js'},
         # {'url': 'https://web-cdn.ttvnw.net/premium.js',
         #  'filename': 'premium.js'},
-        {'url': 'https://www.twitch.tv/tmilibs/tmi-v3.js',
-         'filename': 'tmi-v3.js'}
+        # {'url': 'https://www.twitch.tv/tmilibs/tmi-v3.js',
+        #  'filename': 'tmi-v3.js'}
     ]
 
     for jsfile in files:
