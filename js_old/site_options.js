aadcd2d0616bcb06c5983e0f659a8b3595cfc156
derrod <hitreg@really.dice.wtf>
2018-01-11 03:16:15 +0100

    Update 11.01.2018 03:16

--- js_old/site_options.js
+++ js_old/site_options.js
@@ -485,10 +485,10 @@ window.SiteOptions = {
         },
         "19cc1327-9436-4019-b71c-e2a1e41d3cb3": {
             "name": "test",
-            "v": 2812,
+            "v": 2813,
             "t": 1,
             "groups": [{
-                "weight": 16,
+                "weight": 18,
                 "value": "a"
             }, {
                 "weight": 2,
