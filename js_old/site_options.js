94462a0a77de1332b036426251c0d258d0aa5faf
derrod <hitreg@really.dice.wtf>
2018-01-19 22:31:14 +0100

    Update 19.01.2018 22:31

--- js_old/site_options.js
+++ js_old/site_options.js
@@ -485,10 +485,10 @@ window.SiteOptions = {
         },
         "19cc1327-9436-4019-b71c-e2a1e41d3cb3": {
             "name": "test",
-            "v": 2854,
+            "v": 2858,
             "t": 1,
             "groups": [{
-                "weight": 10,
+                "weight": 5,
                 "value": "a"
             }, {
                 "weight": 2,
