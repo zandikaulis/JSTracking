c6376ef8d191fee9b3d9b3e4f5cfa2e8200fc183
derrod <hitreg@really.dice.wtf>
2018-01-29 23:46:14 +0100

    Update 29.01.2018 23:46

--- js/player/player.js
+++ js/player/player.js
@@ -38,7 +38,7 @@
             r[2] = o;
             var a = document.getElementsByTagName("head")[0],
                 s = document.createElement("script");
-            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".6894f6521bdf7031272d.js";
+            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".3453fb4c94e7e2d2abe7.js";
             var u = setTimeout(n, 12e4);
             return s.onerror = s.onload = n, a.appendChild(s), o
         }, t.m = e, t.c = r, t.d = function(e, n, r) {
@@ -27860,7 +27860,7 @@
                     u = Ra.a(!0),
                     c = Ia.u() === Ia.j ? null : Da;
                 return {
-                    app_version: "2018.01.29-192008+82d26cdfe50561cce309500c96df7253e828b53a",
+                    app_version: "2018.01.29-215547+5f2bbfd882439d676160c29a26f8a7525daa6ab9",
                     flash_version: r,
                     referrer_url: i,
                     referrer_host: o.host,
