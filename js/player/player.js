8f990a52e4d533cf3c09900584498c816a4808b7
derrod <hitreg@really.dice.wtf>
2018-01-30 23:31:10 +0100

    Update 30.01.2018 23:31

--- js/player/player.js
+++ js/player/player.js
@@ -38,7 +38,7 @@
             r[2] = o;
             var a = document.getElementsByTagName("head")[0],
                 s = document.createElement("script");
-            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".7897d5ce9c622d825372.js";
+            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".33f00b2a8effa8bea8d8.js";
             var u = setTimeout(n, 12e4);
             return s.onerror = s.onload = n, a.appendChild(s), o
         }, t.m = e, t.c = r, t.d = function(e, n, r) {
@@ -27859,7 +27859,7 @@
                     u = Ra.a(!0),
                     c = Ia.u() === Ia.j ? null : Da;
                 return {
-                    app_version: "2018.01.30-012747+3a83b32b0d9e4909c2ec84b9a4090a6c2368f260",
+                    app_version: "2018.01.30-215407+2f1f20c1371996841c112586bb9db435a40a2fc7",
                     flash_version: r,
                     referrer_url: i,
                     referrer_host: o.host,
