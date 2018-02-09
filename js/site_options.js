8427aeaa2ec906707ab09e38c105bbf681f1f043
derrod <hitreg@really.dice.wtf>
2018-02-09 20:46:23 +0100

    Update 09.02.2018 20:46

--- js/site_options.js
+++ js/site_options.js
@@ -4976,7 +4976,7 @@ window.SiteOptions = {
         },
         "fd5223cb-3ae7-4a9f-9af8-ce0d44d4b9f1": {
             "name": "pad_thai",
-            "v": 2994,
+            "v": 3027,
             "t": 1,
             "groups": [{
                 "weight": 95,
