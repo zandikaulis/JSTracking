63750a76d31d3c73b9b92b10d575f181c88edea7
derrod <hitreg@really.dice.wtf>
2018-02-09 22:16:19 +0100

    Update 09.02.2018 22:16

--- js/site_options.js
+++ js/site_options.js
@@ -4976,14 +4976,26 @@ window.SiteOptions = {
         },
         "fd5223cb-3ae7-4a9f-9af8-ce0d44d4b9f1": {
             "name": "pad_thai",
-            "v": 3027,
+            "v": 3028,
             "t": 1,
             "groups": [{
                 "weight": 95,
                 "value": "control"
             }, {
-                "weight": 5,
+                "weight": 1,
                 "value": "treatment"
+            }, {
+                "weight": 1,
+                "value": "agrobuf"
+            }, {
+                "weight": 1,
+                "value": "lowbuf"
+            }, {
+                "weight": 1,
+                "value": "maxbuf"
+            }, {
+                "weight": 1,
+                "value": "offseg"
             }]
         },
         "fd74ad1d-d885-4d12-9036-f571fc82e6df": {
