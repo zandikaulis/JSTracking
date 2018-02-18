0f6fdebd60f6ecd7f53bdf70ea2c7f76eb800e0b
derrod <hitreg@really.dice.wtf>
2018-02-18 19:31:25 +0100

    Update 18.02.2018 19:31

--- js/assets/core.js
+++ js/assets/core.js
@@ -13845,7 +13845,6 @@ webpackJsonp([68], {
                         }),
                         queryDeduplication: !0,
                         link: new F.a({
-                            batchMax: 20,
                             fetch: this.createApolloFetcher()
                         })
                     })
@@ -40164,4 +40163,4 @@ webpackJsonp([68], {
             }(r.Component))
     }
 }, [4]);
-//# sourceMappingURL=core-3b36f61ab95c1dfb5c07fd638679fc29.js.map
+//# sourceMappingURL=core-b72289bea70a3f9599d730acf4d7567a.js.map
