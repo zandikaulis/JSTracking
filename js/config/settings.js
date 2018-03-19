167e902d8d7f3dc83a2de145b03e23b519266d2c
derrod <hitreg@really.dice.wtf>
2018-03-19 18:16:39 +0100

    Update 19.03.2018 18:16

--- js/config/settings.js
+++ js/config/settings.js
@@ -27,6 +27,7 @@ window.__twilightSettings = {
     "esport_insider_pass_whitelist": [],
     "event_directory_enabled_games": ["overwatch", "twitchcon 2017", "twitch presents", "dota 2", "league of legends", "call of duty: wwii", "rocket league", "tekken 7", "talk shows", "always on", "hackweek esports"],
     "extensions_mobile_banner_enabled": false,
+    "first_page_loaded_timeout": 10000,
     "google_analytics_url": "https://ssl.google-analytics.com/ga.js",
     "impact_experiment_key": true,
     "ivw_countries": ["DE"],
