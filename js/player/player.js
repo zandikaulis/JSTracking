40b20a6d110f61a12acd4a9f4a7b7e1d0ca8b78a
derrod <hitreg@really.dice.wtf>
2018-03-20 01:46:35 +0100

    Update 20.03.2018 01:46

--- js/player/player.js
+++ js/player/player.js
@@ -38,7 +38,7 @@
             r[2] = o;
             var a = document.getElementsByTagName("head")[0],
                 s = document.createElement("script");
-            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".6fb8b72cad990f9aecd2.js";
+            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".9c1eb8897284ac09fa1e.js";
             var u = setTimeout(n, 12e4);
             return s.onerror = s.onload = n, a.appendChild(s), o
         }, t.m = e, t.c = r, t.d = function(e, n, r) {
@@ -30313,7 +30313,7 @@
                     h = d.os_name,
                     m = d.os_version;
                 return {
-                    app_version: "2018.03.19-190657+13122a7fc216edc3121984aacec4e87bd3c85a2b",
+                    app_version: "2018.03.19-232029+95d59cebcc744b1e109609c124b614530b139c0a",
                     flash_version: r,
                     referrer_url: i,
                     referrer_host: o.host,
