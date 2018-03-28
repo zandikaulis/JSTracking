98d7cb658ea7399aad1f3bf6c845b9e0edcec4c0
derrod <hitreg@really.dice.wtf>
2018-03-28 04:31:51 +0200

    Update 28.03.2018 04:31

--- js/site_options.js
+++ js/site_options.js
@@ -2485,7 +2485,7 @@ window.SiteOptions = {
         },
         "697896b3-a4b9-4c00-a1a2-7fa35ec9a8e4": {
             "name": "pushy_notifications_disable",
-            "v": 3366,
+            "v": 3393,
             "t": 2,
             "groups": [{
                 "weight": 100,
