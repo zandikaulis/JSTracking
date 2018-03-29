0c11ca024d844d9c0d4cc04e32fcb99252ef0fa6
derrod <hitreg@really.dice.wtf>
2018-03-30 00:16:44 +0200

    Update 30.03.2018 00:16

--- js/player/player.js
+++ js/player/player.js
@@ -38,7 +38,7 @@
             r[2] = a;
             var o = document.getElementsByTagName("head")[0],
                 s = document.createElement("script");
-            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".4e30fcc6c8518cbd7015.js";
+            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".6612de93604c8c2f2a6a.js";
             var u = setTimeout(n, 12e4);
             return s.onerror = s.onload = n, o.appendChild(s), a
         }, t.m = e, t.c = r, t.d = function(e, n, r) {
@@ -31006,7 +31006,7 @@
                     h = d.os_name,
                     m = d.os_version;
                 return {
-                    app_version: "2018.03.29-212836+2b585c7d9a08c532081ae1704f2cf5bd4b6e18db",
+                    app_version: "2018.03.29-215448+c7429cc42d4650eab41651eca328065f441e956e",
                     flash_version: r,
                     referrer_url: i,
                     referrer_host: s.host,
