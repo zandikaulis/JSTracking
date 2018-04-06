df1d4c3127100e0f753d896a4f91d297c589920f
derrod <hitreg@really.dice.wtf>
2018-04-06 23:01:44 +0200

    Update 06.04.2018 23:01

--- js/player/player.js
+++ js/player/player.js
@@ -38,7 +38,7 @@
             r[2] = o;
             var a = document.getElementsByTagName("head")[0],
                 s = document.createElement("script");
-            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".5501f915f091179874c6.js";
+            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".d6aa50ade0e0d8032a6e.js";
             var u = setTimeout(n, 12e4);
             return s.onerror = s.onload = n, a.appendChild(s), o
         }, t.m = e, t.c = r, t.d = function(e, n, r) {
@@ -31162,7 +31162,7 @@
                     h = d.os_name,
                     m = d.os_version;
                 return {
-                    app_version: "2018.04.06-190901+7a38ce914be2949cbb88cfe8683ec3a55bc0e126",
+                    app_version: "2018.04.06-203243+556cbf6f25a3d405b274d9c924d7f2c8a92d4352",
                     flash_version: r,
                     referrer_url: i,
                     referrer_host: s.host,
@@ -36378,7 +36378,7 @@
                 muted: !1,
                 controls: !0,
                 autoplay: !0,
-                playsinline: !1,
+                playsinline: !0,
                 quality: "medium",
                 time: null,
                 leaveDialogEnabled: wa.w.enabled,
