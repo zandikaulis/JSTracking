c81717cea3d9fed3390c3658386976c1274514da
derrod <hitreg@really.dice.wtf>
2018-04-09 23:01:49 +0200

    Update 09.04.2018 23:01

--- js/player/player.js
+++ js/player/player.js
@@ -38,7 +38,7 @@
             r[2] = o;
             var a = document.getElementsByTagName("head")[0],
                 s = document.createElement("script");
-            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".d6aa50ade0e0d8032a6e.js";
+            s.type = "text/javascript", s.charset = "utf-8", s.async = !0, s.timeout = 12e4, t.nc && s.setAttribute("nonce", t.nc), s.src = t.p + "js/" + e + ".cd87116549fa12cc1948.js";
             var u = setTimeout(n, 12e4);
             return s.onerror = s.onload = n, a.appendChild(s), o
         }, t.m = e, t.c = r, t.d = function(e, n, r) {
@@ -31162,7 +31162,7 @@
                     h = d.os_name,
                     m = d.os_version;
                 return {
-                    app_version: "2018.04.06-203243+556cbf6f25a3d405b274d9c924d7f2c8a92d4352",
+                    app_version: "2018.04.06-212141+b1d0cd329c2781f68e93cd1ab8bc57c01978fb7e",
                     flash_version: r,
                     referrer_url: i,
                     referrer_host: s.host,
