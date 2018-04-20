2ac1c7afb6cbb68cbb75e940f56dd1ef37b47ec9
derrod <hitreg@really.dice.wtf>
2018-04-20 17:01:54 +0200

    Update 20.04.2018 17:01

--- js/config/settings.js
+++ js/config/settings.js
@@ -92,7 +92,7 @@ window.__twilightSettings = {
     "twitch_prime_fortnite_link_url": "https://www.epicgames.com/fortnite/twitch-prime-pack2",
     "twitch_prime_fortnite_signup_url": "https://twitch.amazon.com/prime/country",
     "update_chunks": ["core.css", "core.js", "pages.anon-front.js", "pages.browse.js", "pages.channel.js", "pages.directory-game.js", "pages.following.js", "pages.front.js", "vendor.js"],
-    "update_manager_enabled": false,
+    "update_manager_enabled": true,
     "viewer_milestones_enabled": true,
     "withgraphql_retry_delay_backoff_multiplier": 3,
     "withgraphql_retry_enabled": true,
