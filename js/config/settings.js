84074e37ed42a8cc0d0cc5e9f92370a16880fcfb
derrod <hitreg@really.dice.wtf>
2018-04-24 18:16:56 +0200

    Update 24.04.2018 18:16

--- js/config/settings.js
+++ js/config/settings.js
@@ -32,7 +32,7 @@ window.__twilightSettings = {
     "esport_insider_pass_secondary_channel": "overwatchleague_allaccess",
     "esport_insider_pass_whitelist": ["137512364", "188863650", "188864445", "27697171-staff"],
     "event_directory_enabled_games": ["overwatch", "twitchcon 2017", "twitch presents", "dota 2", "league of legends", "call of duty: wwii", "rocket league", "tekken 7", "talk shows", "always on", "wrestling"],
-    "extensions_bits_banner_enabled": false,
+    "extensions_bits_banner_enabled": true,
     "extensions_mobile_banner_enabled": false,
     "first_page_loaded_timeout": 30000,
     "google_analytics_url": "https://ssl.google-analytics.com/ga.js",
