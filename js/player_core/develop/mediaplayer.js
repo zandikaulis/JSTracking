8e9201b00f43328e157700a90c89868891940790
derrod <hitreg@really.dice.wtf>
2018-05-16 15:02:33 +0200

    Update 16.05.2018 15:02

--- js/player_core/develop/mediaplayer.js
+++ js/player_core/develop/mediaplayer.js
@@ -2569,7 +2569,7 @@ MediaPlayer.prototype.getVideoBitRate = function () {
 }
 
 MediaPlayer.prototype.getVersion = function () {
-    return "2.3.0-1536cad5";
+    return "2.3.0-fedafeeb";
 }
 
 MediaPlayer.prototype.isLooping = function () {
