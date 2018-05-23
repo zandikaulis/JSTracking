cb666bffb62fab6adc20e209769802a3cecd2895
derrod <hitreg@really.dice.wtf>
2018-05-23 21:47:08 +0200

    Update 23.05.2018 21:47

--- js/config/settings.js
+++ js/config/settings.js
@@ -31,7 +31,7 @@ window.__twilightSettings = {
     "dashboard_vp_widget_enabled": true,
     "deprecated_messages_download_enabled": true,
     "detector_urls": ["https://s0.2mdn.net/", "https://adservice.google.com", "https://pagead2.googlesyndication.com/", "https://r.nexac.com/e/getdata.xgi?foo=bar"],
-    "env_test": "This is a test in Production",
+    "env_test": "This is a test in Production.",
     "esport_channel_page_whitelist": ["137512364", "188863650", "188864445", "27697171"],
     "esport_insider_pass_gifting_launch": "on",
     "esport_insider_pass_secondary_channel": "overwatchleague_allaccess",
