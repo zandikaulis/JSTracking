a1b482bd7b6c57392cedad8a7a7cefb5affbf61b
derrod <hitreg@really.dice.wtf>
2018-05-29 04:00:32 +0200

    Update 29.05.2018 04:00

--- js/config/settings.js
+++ js/config/settings.js
@@ -29,7 +29,7 @@ window.__twilightSettings = {
     "dashboard_moderation_settings_enabled": true,
     "dashboard_settings_launch": "on",
     "dashboard_vp_widget_enabled": true,
-    "deprecated_messages_download_enabled": true,
+    "deprecated_messages_download_enabled": false,
     "detector_urls": ["https://s0.2mdn.net/", "https://adservice.google.com", "https://pagead2.googlesyndication.com/", "https://r.nexac.com/e/getdata.xgi?foo=bar"],
     "env_test": "This is a test in Production.",
     "esport_channel_page_whitelist": ["137512364", "188863650", "188864445", "27697171"],
