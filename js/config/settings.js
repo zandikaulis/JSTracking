9f0d9a61c14be1b34744c3c41009e8c59502d514
derrod <hitreg@really.dice.wtf>
2018-06-02 00:45:45 +0200

    Update 02.06.2018 00:45

--- js/config/settings.js
+++ js/config/settings.js
@@ -481,14 +481,14 @@ window.__twilightSettings = {
         },
         "50e947d4-24b7-4000-a046-f48f4072b7cf": {
             "name": "TWILIGHT_SUBSCRIBED_CHANNELS_SIDENAV",
-            "v": 3903,
+            "v": 3904,
             "t": 2,
             "s": 1,
             "groups": [{
-                "weight": 60,
+                "weight": 50,
                 "value": "control"
             }, {
-                "weight": 30,
+                "weight": 40,
                 "value": "variant_a"
             }, {
                 "weight": 10,
--- js/player_core/develop/mediaplayer.js
+++ js/player_core/develop/mediaplayer.js
@@ -2573,7 +2573,7 @@ MediaPlayer.prototype.getVideoBitRate = function () {
 }
 
 MediaPlayer.prototype.getVersion = function () {
-    return "2.3.0-1cdf878b";
+    return "2.3.0-6f7d3bf0";
 }
 
 MediaPlayer.prototype.isLooping = function () {
