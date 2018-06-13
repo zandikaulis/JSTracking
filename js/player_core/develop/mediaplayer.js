e88a1211b0cb60a1727c3bd0d3e6518c96162e6c
derrod <hitreg@really.dice.wtf>
2018-06-14 01:31:07 +0200

    Update 14.06.2018 01:31

--- js/player_core/develop/mediaplayer.js
+++ js/player_core/develop/mediaplayer.js
@@ -2584,7 +2584,7 @@ MediaPlayer.prototype.getVideoBitRate = function () {
 }
 
 MediaPlayer.prototype.getVersion = function () {
-    return "2.3.0-874fe88b";
+    return "2.3.0-82ea1bb6";
 }
 
 MediaPlayer.prototype.isLooping = function () {
