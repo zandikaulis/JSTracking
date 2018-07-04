d24f5c2e4f99246abd5f14d198f5f0ef29965d84
derrod <hitreg@really.dice.wtf>
2018-07-04 05:30:37 +0200

    Update 04.07.2018 05:30

--- js/player_core/develop/mediaplayer.js
+++ js/player_core/develop/mediaplayer.js
@@ -2595,7 +2595,7 @@ MediaPlayer.prototype.getVideoBitRate = function () {
 }
 
 MediaPlayer.prototype.getVersion = function () {
-    return "2.3.0-cce296a9";
+    return "2.3.0-d28f571c";
 }
 
 MediaPlayer.prototype.isLooping = function () {
