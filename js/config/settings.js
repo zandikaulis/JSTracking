3fae2092309a2c98aee3179c05b925b926141da4
derrod <hitreg@really.dice.wtf>
2018-07-24 21:45:40 +0200

    Update 24.07.2018 21:45

--- js/config/settings.js
+++ js/config/settings.js
@@ -1321,14 +1321,14 @@ window.__twilightSettings = {
         },
         "ec70bd08-dbc8-4f48-9f60-d035fec3eb20": {
             "name": "CVP Player Core 2.0 Mediaplayer Experiment Aug 2017",
-            "v": 4260,
+            "v": 4290,
             "t": 1,
             "groups": [{
                 "weight": 95,
                 "value": "2.5.16"
             }, {
                 "weight": 5,
-                "value": "2.5.19"
+                "value": "2.5.20"
             }]
         },
         "ee491c3c-d6f7-4f00-8c53-c630dfa10f41": {
