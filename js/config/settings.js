807c55a8472db1f577d1d47475b3bd332adf355d
derrod <hitreg@really.dice.wtf>
2018-07-25 01:45:40 +0200

    Update 25.07.2018 01:45

--- js/config/settings.js
+++ js/config/settings.js
@@ -538,13 +538,13 @@ window.__twilightSettings = {
         },
         "462fee3f-7c32-430d-854f-c31707139b12": {
             "name": "twilight_live_dashboard_stats_clips_polling_rate",
-            "v": 4278,
+            "v": 4294,
             "t": 2,
             "groups": [{
-                "weight": 80,
+                "weight": 50,
                 "value": "control"
             }, {
-                "weight": 20,
+                "weight": 50,
                 "value": "30-second-polling-rate-experiment"
             }]
         },
