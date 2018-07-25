220efdc06c3eb127e50a5398f4d05a5e9c78ae38
derrod <hitreg@really.dice.wtf>
2018-07-25 23:00:46 +0200

    Update 25.07.2018 23:00

--- js/config/settings.js
+++ js/config/settings.js
@@ -1082,19 +1082,19 @@ window.__twilightSettings = {
         },
         "b40e065c-7a73-4c4f-b060-a6b1647fc1a2": {
             "name": "twilight_editing_im_upset",
-            "v": 4271,
+            "v": 4307,
             "t": 1,
             "groups": [{
-                "weight": 100,
+                "weight": 25,
                 "value": "control"
             }, {
-                "weight": 0,
+                "weight": 25,
                 "value": "variant1"
             }, {
-                "weight": 0,
+                "weight": 25,
                 "value": "variant2"
             }, {
-                "weight": 0,
+                "weight": 25,
                 "value": "variant3"
             }]
         },
