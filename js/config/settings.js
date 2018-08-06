0f0e56665f087909219e885e57680cb81dab2663
derrod <hitreg@really.dice.wtf>
2018-08-06 21:45:30 +0200

    Update 06.08.2018 21:45

--- js/config/settings.js
+++ js/config/settings.js
@@ -4,6 +4,7 @@ window.__twilightSettings = {
     "benchmark_component_threshold": 0.1,
     "benchmark_networking_threshold": 0.15,
     "better_together": "on",
+    "bits_automod_cheering_enabled": "off",
     "bits_prime_offer": false,
     "bits_special_promo_base_payout": 1000,
     "bits_special_promo_bonus_payout": 0,
