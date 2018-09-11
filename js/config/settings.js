932a0ab682b002fd1ddbf4da575959e85012a909
derrod <hitreg@really.dice.wtf>
2018-09-11 23:15:38 +0200

    Update 11.09.2018 23:15

--- js/config/settings.js
+++ js/config/settings.js
@@ -57,6 +57,7 @@ window.__twilightSettings = {
     "esport_insider_prime_bits_launch": "on",
     "esport_insider_prime_bits_launch_user_whitelist": ["186243532", "215590603"],
     "esports_room_cheering_whitelist": ["137512364", "188863650", "188864445", "27697171", "191316249-staff", "104874624"],
+    "eu_respawn_enabled": "off",
     "event_directory_enabled_games": [],
     "extensions_mve_banner_enabled": true,
     "extensions_popout_enabled": "on",
