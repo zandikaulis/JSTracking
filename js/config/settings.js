7476324f874b82cb433ce0dc2c1449c82246dda4
derrod <hitreg@really.dice.wtf>
2018-10-02 02:15:37 +0200

    Update 02.10.2018 02:15

--- js/config/settings.js
+++ js/config/settings.js
@@ -570,11 +570,11 @@ window.__twilightSettings = {
         },
         "3c3a5d48-117f-43ac-8b35-091f13b2d479": {
             "name": "MediaPlayer Staff Version",
-            "v": 4755,
+            "v": 4767,
             "t": 1,
             "groups": [{
                 "weight": 100,
-                "value": "2.6.20"
+                "value": "2.6.22"
             }]
         },
         "3f03eebb-3cbb-4f7c-b64b-e1ed0115a773": {
@@ -1572,11 +1572,14 @@ window.__twilightSettings = {
         },
         "ec70bd08-dbc8-4f48-9f60-d035fec3eb20": {
             "name": "CVP Player Core 2.0 Mediaplayer Experiment Aug 2017",
-            "v": 4754,
+            "v": 4768,
             "t": 1,
             "groups": [{
-                "weight": 100,
+                "weight": 95,
                 "value": "2.6.20"
+            }, {
+                "weight": 5,
+                "value": "2.6.22"
             }]
         },
         "ee491c3c-d6f7-4f00-8c53-c630dfa10f41": {
