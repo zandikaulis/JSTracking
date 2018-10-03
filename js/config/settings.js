6b5e187ce35cd3f4ca3bfcfbf2fab0d1d32d6988
derrod <hitreg@really.dice.wtf>
2018-10-03 20:30:42 +0200

    Update 03.10.2018 20:30

--- js/config/settings.js
+++ js/config/settings.js
@@ -570,11 +570,11 @@ window.__twilightSettings = {
         },
         "3c3a5d48-117f-43ac-8b35-091f13b2d479": {
             "name": "MediaPlayer Staff Version",
-            "v": 4780,
+            "v": 4786,
             "t": 1,
             "groups": [{
                 "weight": 100,
-                "value": "2.6.23"
+                "value": "2.6.24"
             }]
         },
         "3f03eebb-3cbb-4f7c-b64b-e1ed0115a773": {
@@ -1572,14 +1572,14 @@ window.__twilightSettings = {
         },
         "ec70bd08-dbc8-4f48-9f60-d035fec3eb20": {
             "name": "CVP Player Core 2.0 Mediaplayer Experiment Aug 2017",
-            "v": 4781,
+            "v": 4787,
             "t": 1,
             "groups": [{
-                "weight": 50,
-                "value": "2.6.20"
-            }, {
-                "weight": 50,
+                "weight": 90,
                 "value": "2.6.23"
+            }, {
+                "weight": 10,
+                "value": "2.6.24"
             }]
         },
         "ee491c3c-d6f7-4f00-8c53-c630dfa10f41": {
