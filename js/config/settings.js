11bbb7eac16d2527aad169dd5d4d7a66faf4d722
derrod <hitreg@really.dice.wtf>
2018-10-13 00:32:35 +0200

    Update 13.10.2018 00:32

--- js/config/settings.js
+++ js/config/settings.js
@@ -41,7 +41,7 @@ window.__twilightSettings = {
     "dashboard_payout_history": true,
     "dashboard_vp_widget_enabled": true,
     "deprecated_messages_download_enabled": true,
-    "detector_urls": ["https://s0.2mdn.net/", "https://adservice.google.com", "https://pagead2.googlesyndication.com/", "https://r.nexac.com/e/getdata.xgi?foo=bar"],
+    "detector_urls": [],
     "dev_extension_sandbox_launch": "on",
     "dev_insights_feature_launch": "on",
     "drops_itemnames_enabled": false,
