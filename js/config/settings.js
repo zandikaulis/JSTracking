bef649a230c72fd4eadbd4b63e45de6ed60dd764
derrod <hitreg@really.dice.wtf>
2018-10-26 21:32:35 +0200

    Update 26.10.2018 21:32

--- js/config/settings.js
+++ js/config/settings.js
@@ -21,7 +21,7 @@ window.__twilightSettings = {
     "bounty_board_available_bounties_date": "Wed June 27 2018 00:00:00 GMT-0700 (PDT)",
     "bounty_board_brand_portal_enabled": "on",
     "bounty_board_live_dashboard_widget": "on",
-    "cc_v2_flag": "off",
+    "cc_v2_flag": "staff",
     "cc_v2_trial_test_enabled": false,
     "cc_v2_whitelist": ["cpt_meticulous-staff", "testnioh123-staff"],
     "channel_feed_enabled": false,
