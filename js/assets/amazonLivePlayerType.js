518b1a9d2d439de9bf36f7b0fc6e84088642d69d
derrod <hitreg@really.dice.wtf>
2018-10-30 11:30:31 +0100

    Update 30.10.2018 11:30

--- js/assets/amazonLivePlayerType.js
+++ js/assets/amazonLivePlayerType.js
@@ -1,5 +1,5 @@
 (window.webpackJsonp = window.webpackJsonp || []).push([
-    [199], {
+    [200], {
         iSLP: function(e, t, n) {
             "use strict";
             Object.defineProperty(t, "__esModule", {
