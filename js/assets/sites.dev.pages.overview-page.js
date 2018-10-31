7f77add264e55b8b8623dbbd30af21af157922d7
derrod <hitreg@really.dice.wtf>
2018-10-31 04:17:37 +0100

    Update 31.10.2018 04:17

--- js/assets/sites.dev.pages.overview-page.js
+++ js/assets/sites.dev.pages.overview-page.js
@@ -1,5 +1,5 @@
 (window.webpackJsonp = window.webpackJsonp || []).push([
-    [207], {
+    [206], {
         "0xbx": function(t, e, n) {
             "use strict";
             n.d(e, "a", function() {
