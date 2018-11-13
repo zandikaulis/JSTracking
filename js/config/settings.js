042117d9f2b301553484d87afb9ce2369c36ef05
derrod <hitreg@really.dice.wtf>
2018-11-14 00:32:58 +0100

    Update 14.11.2018 00:32

--- js/config/settings.js
+++ js/config/settings.js
@@ -602,11 +602,11 @@ window.__twilightSettings = {
         },
         "3c3a5d48-117f-43ac-8b35-091f13b2d479": {
             "name": "MediaPlayer Staff Version",
-            "v": 5122,
+            "v": 5134,
             "t": 1,
             "groups": [{
                 "weight": 100,
-                "value": "2.6.33+abrprobe"
+                "value": "2.6.34+abrprobe"
             }]
         },
         "3f03eebb-3cbb-4f7c-b64b-e1ed0115a773": {
